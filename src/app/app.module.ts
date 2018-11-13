d8070c3608c93420b0945aa5a184cfc4ca1ad1b1
Rodrigo Grisolía <grisoliarodrigo@gmail.com>
2018-11-12 23:07:07 -0300

    upload inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus, faShoppingCart, faMinus } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlus, faShoppingCart, faMinus)
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
